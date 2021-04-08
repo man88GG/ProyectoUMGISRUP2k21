c92bba5cac532cd442a1a2f488c026bebbd6089e
Jose09011712 <jlopezt11@miumg.edu.gt>
2021-04-08 15:28:54 -0600

    Estructura
    Codigo de la clase conexion agregado.

--- MODULO_HRM/HRM/CapaModeloHRM/ClsConexion.cs
+++ MODULO_HRM/HRM/CapaModeloHRM/ClsConexion.cs
@@ -9,5 +9,32 @@ namespace CapaModeloHRM
 {
     class ClsConexion
     {
+        public OdbcConnection conexion()
+        {
+            //creacion de la conexion via ODBC
+            OdbcConnection conn = new OdbcConnection("Dsn=Predeterminado");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return conn;
+        }
+
+        //metodo para cerrar la conexion
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+        }
     }
 }
