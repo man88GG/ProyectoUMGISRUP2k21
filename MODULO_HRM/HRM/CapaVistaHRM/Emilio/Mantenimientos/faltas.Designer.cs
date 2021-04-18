c850baebde2c3bfb61fc1c34f2a4e7d164927db0
Jose09011712 <jlopezt11@miumg.edu.gt>
2021-04-18 16:54:28 -0600

    Mantenimientos Terminados

--- MODULO_HRM/HRM/CapaVistaHRM/Emilio/Mantenimientos/faltas.Designer.cs
+++ MODULO_HRM/HRM/CapaVistaHRM/Emilio/Mantenimientos/faltas.Designer.cs
@@ -48,63 +48,86 @@ namespace CapaVistaHRM.Emilio.Mantenimientos
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(394, 116);
+            this.dataGridView1.Location = new System.Drawing.Point(525, 143);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(675, 217);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(900, 267);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 123);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(43, 151);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.Size = new System.Drawing.Size(61, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Codigo";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 146);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(43, 180);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.Size = new System.Drawing.Size(106, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tipo de Falta";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 175);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(43, 215);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.Size = new System.Drawing.Size(99, 20);
             this.label3.TabIndex = 3;
             this.label3.Text = "Descripcion";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 262);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(43, 322);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.Size = new System.Drawing.Size(48, 20);
             this.label4.TabIndex = 4;
             this.label4.Text = "Valor";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(32, 336);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(43, 414);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.Size = new System.Drawing.Size(61, 20);
             this.label5.TabIndex = 5;
             this.label5.Text = "Estado";
             // 
             // Activo
             // 
             this.Activo.AutoSize = true;
-            this.Activo.Location = new System.Drawing.Point(138, 336);
+            this.Activo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Activo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Activo.Location = new System.Drawing.Point(184, 414);
+            this.Activo.Margin = new System.Windows.Forms.Padding(4);
             this.Activo.Name = "Activo";
-            this.Activo.Size = new System.Drawing.Size(55, 17);
+            this.Activo.Size = new System.Drawing.Size(76, 24);
             this.Activo.TabIndex = 6;
             this.Activo.TabStop = true;
             this.Activo.Text = "Activo";
@@ -114,9 +137,12 @@ namespace CapaVistaHRM.Emilio.Mantenimientos
             // Inactivo
             // 
             this.Inactivo.AutoSize = true;
-            this.Inactivo.Location = new System.Drawing.Point(246, 334);
+            this.Inactivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Inactivo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Inactivo.Location = new System.Drawing.Point(328, 411);
+            this.Inactivo.Margin = new System.Windows.Forms.Padding(4);
             this.Inactivo.Name = "Inactivo";
-            this.Inactivo.Size = new System.Drawing.Size(63, 17);
+            this.Inactivo.Size = new System.Drawing.Size(87, 24);
             this.Inactivo.TabIndex = 7;
             this.Inactivo.TabStop = true;
             this.Inactivo.Text = "Inactivo";
@@ -125,43 +151,53 @@ namespace CapaVistaHRM.Emilio.Mantenimientos
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(147, 116);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(196, 143);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(210, 20);
+            this.textBox1.Size = new System.Drawing.Size(279, 27);
             this.textBox1.TabIndex = 8;
-            this.textBox1.Tag = "IdTipoFalta";
+            this.textBox1.Tag = "idTipoFalta";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(147, 146);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(196, 180);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(210, 20);
+            this.textBox2.Size = new System.Drawing.Size(279, 27);
             this.textBox2.TabIndex = 9;
-            this.textBox2.Tag = "tipofalta";
+            this.textBox2.Tag = "tipoFalta";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(147, 175);
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(196, 215);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox3.Size = new System.Drawing.Size(210, 81);
+            this.textBox3.Size = new System.Drawing.Size(279, 99);
             this.textBox3.TabIndex = 10;
             this.textBox3.Tag = "descripcion";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(147, 262);
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(196, 322);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(210, 20);
+            this.textBox4.Size = new System.Drawing.Size(279, 27);
             this.textBox4.TabIndex = 11;
             this.textBox4.Tag = "valor";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(147, 297);
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(196, 366);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(210, 20);
+            this.textBox5.Size = new System.Drawing.Size(279, 27);
             this.textBox5.TabIndex = 12;
             this.textBox5.Tag = "estado";
             this.textBox5.Visible = false;
@@ -170,17 +206,19 @@ namespace CapaVistaHRM.Emilio.Mantenimientos
             // navegador1
             // 
             this.navegador1.BackColor = System.Drawing.Color.Transparent;
-            this.navegador1.Location = new System.Drawing.Point(12, -4);
+            this.navegador1.Location = new System.Drawing.Point(16, -5);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(5);
             this.navegador1.Name = "navegador1";
-            this.navegador1.Size = new System.Drawing.Size(1059, 105);
+            this.navegador1.Size = new System.Drawing.Size(1412, 129);
             this.navegador1.TabIndex = 13;
             this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
             // 
             // faltas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1081, 372);
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(1441, 458);
             this.Controls.Add(this.navegador1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -195,7 +233,11 @@ namespace CapaVistaHRM.Emilio.Mantenimientos
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
             this.Name = "faltas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "faltas";
             this.Load += new System.EventHandler(this.faltas_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
